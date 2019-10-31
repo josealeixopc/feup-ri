969fd5259bd7ee3abcba180f0c348e8b3c60cdb6
José Aleixo Cruz <josealeixo.pc@gmail.com>
2019-10-31 12:21:11 +0000

    Fixed diff drive model deleting problem

--- assing2/catkin_ws/src/reactive_bot_description/scripts/controller.py
+++ assing2/catkin_ws/src/reactive_bot_description/scripts/controller.py
@@ -9,22 +9,31 @@ from geometry_msgs.msg import Twist
 # from datetime import datetime
 
 # Util imports
-import random
-import math
-import time
+# import random
+# import math
+# import time
 import platform
-import sys
+# import sys
 
 # The topic to which the sensor's plugin is publihing results
 # (from the gazebo.xacro file)
 SENSOR_TOPIC = '/reactive_bot/laser/scan'
 CONTROLLER_TOPIC = '/reactive_bot/controller/position'
 
-# The sensor update rate in Hz (from the gazebo.xacro file)
+# The sensor definitions (from the reactive_bot.xacro file,
+# sensor definition)
 SENSOR_UPDATE_RATE = 20
+SENSOR_HORIZONTAL_SAMPLING = 1080
+SENSOR_HORIZONTAL_RESOLUTION = 1
+SENSOR_HORIZONTAL_MIN_ANGLE = -3.14  # (in radians)
+SENSOR_HORIZONTAL_MAX_ANGLE = 3.14  # (in radians)
+SENSOR_RANGE_MIN = 0.1  # (in meters)
+SENSOR_RANGE_MAX = 10   # (in meters)
+SENSOR_RANGE_RESOLUTION = 0.001
 
 
 def laser_scan_callback(message):
+    print(message)
     pass
 
 
@@ -33,7 +42,7 @@ def main():
     print("Running controller script with Python version: {}".format(
         platform.python_version()))
 
-    print(sys.executable)
+    rospy.init_node('controller_node')
 
     print('Creating publisher and subscriber...')
 
@@ -47,6 +56,28 @@ def main():
 
     print(laser_scan_subscriber)
     print(controller_publisher)
+
+    print('Executing controller...')
+
+    num_iterations = 0
+    rate = rospy.Rate(SENSOR_UPDATE_RATE)
+    while not rospy.is_shutdown():
+        num_iterations = num_iterations + 1
+        controller_msg = Twist()
+
+        # # State Dispatcher
+        # if state_ == 0:
+        #     msg = random_wandering()
+        # elif state_ == 1:
+        #     msg = following_wall()
+        # elif state_ == 2:
+        #     msg = rotating()
+        # else:
+        #     rospy.logerr('Unknown state!')
+
+        controller_publisher.publish(controller_msg)
+
+        rate.sleep()
 
 
 if __name__ == '__main__':
