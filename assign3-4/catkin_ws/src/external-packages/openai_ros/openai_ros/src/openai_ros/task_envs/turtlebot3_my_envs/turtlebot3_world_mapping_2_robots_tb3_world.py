2a783a440b2e3c23fc8fbcad78c45b6243d5bbc5
José Aleixo Cruz <josealeixo.pc@gmail.com>
2020-01-29 11:31:36 +0000

    Updated launch files to work with env variables.

--- assign3-4/catkin_ws/src/external-packages/openai_ros/openai_ros/src/openai_ros/task_envs/turtlebot3_my_envs/turtlebot3_world_mapping_2_robots_tb3_world.py
+++ assign3-4/catkin_ws/src/external-packages/openai_ros/openai_ros/src/openai_ros/task_envs/turtlebot3_my_envs/turtlebot3_world_mapping_2_robots_tb3_world.py
@@ -1,5 +1,16 @@
+import os
+
 import turtlebot3_world_mapping_2_robots
 
 class TurtleBot3WorldMapping2RobotsTB3WorldEnv(turtlebot3_world_mapping_2_robots.TurtleBot3WorldMapping2RobotsEnv):
     def __init__(self):
-         super(TurtleBot3WorldMapping2RobotsTB3WorldEnv, self).__init__("turtlebot3_world_mapping_tb3_world.yaml")
+        # Export TB3 robots position to ENV variable so they can be accessible in the launch file
+        os.environ["FIRST_TB3_X"] = "2.0"
+        os.environ["FIRST_TB3_Y"] = "0.0"
+        os.environ["FIRST_TB3_Z"] = "0.0"
+    
+        os.environ["SECOND_TB3_X"] = "-2.0"
+        os.environ["SECOND_TB3_Y"] = "0.0"
+        os.environ["SECOND_TB3_Z"] = "0.0"
+
+        super(TurtleBot3WorldMapping2RobotsTB3WorldEnv, self).__init__("turtlebot3_world_mapping_tb3_world.yaml")
